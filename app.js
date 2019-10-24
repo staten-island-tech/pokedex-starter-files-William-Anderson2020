3fb6633474c00043705856c72fe8c934664478b6
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-10-24 11:47:05 -0400

    Finish
    Next and prev. buttons if I feel like it

--- app.js
+++ app.js
@@ -7,21 +7,29 @@ const DOMStrings = {
     displayImageShinyFront: document.querySelector('.display-image-shiny-front'),
     displayImageShinyBack: document.querySelector('.display-image-shiny-back'),
     displayNum: document.querySelector('.pkmn-num'),
-    displayImageBanner: document.querySelector('.display-image-banner')
+    displayImageBanner: document.querySelector('.display-image-banner'),
+    displayType: document.querySelector('.type')
 }
 
 function getPkmn(){
     DOMStrings.form.addEventListener('submit', async function(e){
         e.preventDefault();
         try{
-            const result = await fetch(
+            let result = await fetch(
                 `https://pokeapi.co/api/v2/pokemon/${DOMStrings.name.value.toLowerCase()}`
             )
-            const data = await result.json();
+            let data = await result.json();
             console.log(data);
             const displayPkmn = function(data){
                 DOMStrings.displayName.innerText = data.species.name;
-                //DOMStrings.displayImageFront.
+                DOMStrings.displayImageFront.src = data.sprites.front_default;
+                DOMStrings.displayImageBack.src = data.sprites.back_default;
+                DOMStrings.displayImageShinyFront.src = data.sprites.front_shiny;
+                DOMStrings.displayImageShinyBack.src = data.sprites.back_shiny;
+                DOMStrings.displayNum.innerText = data.id;
+                DOMStrings.displayType.innerText = data.types.map(el => el.type.name);
+
+                console.log(data.types);
             }
             displayPkmn(data);
         }catch(err){
@@ -31,3 +39,19 @@ function getPkmn(){
 }
 
 getPkmn();
+
+/* function lastPage(){
+    let result = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${(data.id) - 1}`
+    )
+    let data = await result.json();
+    displayPkmn(data);
+}
+
+function nextPage(){
+    let result = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${(data.id) + 1}`
+    )
+    let data = await result.json();
+    displayPkmn(data);
+} */
